90d1a22b2dcf4e39fa00b529db45967b53469392
Amruta Deshmukh <amrutadeshmukh3116@gmail.com>
2026-02-28 16:51:14 +0530

    testing new

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-# we are some
+# [app.py](http://_vscodecontentref_/3)
+import pandas as pd
+
+# URL of the CSV file on GitHub
+url = (
+    "https://raw.githubusercontent.com/"
+    "justinjohn0306/Sample-Superstore-Data/main/SampleSuperstore.csv"
+)
+
+# read the data
+df = pd.read_excel(r"C:\Users\LENOVO\Downloads\sample_-_superstore.xls")
+
+# quick look
+print(df.head())
+print(df.describe())
+
+# ... perform cleaning, EDA, plotting, etc. ...
